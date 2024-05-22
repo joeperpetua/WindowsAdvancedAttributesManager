f3fce8b601faff6d9f8e5976899e218330eff218
joeperpetua <qwerty.perpetua@gmail.com>
2024-05-23 01:27:46 +0200

    UI boiler plate

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,14 +12,27 @@ using System.Windows.Shapes;
 
 namespace AdvancedAttributesChanger
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TogglePanel(object sender, RoutedEventArgs e) {
+            RadioButton selected = (RadioButton)sender;
+            
+            if (selected == null) { return; }
+
+            if (selected.Name.Equals("RunFile")) {
+                DirectoryPanel.Visibility = Visibility.Collapsed;
+                SingleFilePanel.Visibility = Visibility.Visible;
+            } else {
+                DirectoryPanel.Visibility = Visibility.Visible;
+                SingleFilePanel.Visibility = Visibility.Collapsed;
+            }
+
+            e.Handled = true;
         }
     }
 }
