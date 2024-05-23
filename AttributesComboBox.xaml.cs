9d6114a0e4567f4351cc7c9598a3c30f86467d44
joeperpetua <qwerty.perpetua@gmail.com>
2024-05-23 05:36:35 +0200

    Implemented UI lists and events handlers

---
+++ AttributesComboBox.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AdvancedAttributesChanger
+{
+    /// <summary>
+    /// Interaction logic for AttributesComboBox.xaml
+    /// </summary>
+    public partial class AttributesComboBox : UserControl
+    {
+        public AttributesComboBox()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register("Command", typeof(RoutedEventHandler), typeof(AttributesComboBox), new PropertyMetadata(null));
+
+        public RoutedEventHandler Command
+        {
+            get { return (RoutedEventHandler)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        private void HandleSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Command?.Invoke(sender, new RoutedEventArgs());
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,6 +17,7 @@ namespace AdvancedAttributesChanger
         public MainWindow()
         {
             InitializeComponent();
+            SingleAddCombo.Command = AddItem;
         }
 
         private void TogglePanel(object sender, RoutedEventArgs e) {
@@ -33,6 +34,109 @@ namespace AdvancedAttributesChanger
             }
 
             e.Handled = true;
+        }
+
+        private DockPanel CreateItem(String text)
+        {
+            DockPanel dockPanel = new DockPanel { Width = 155, Margin = new Thickness(5) };
+
+            // button is added first so that it takes the rightmost position.
+            Button button = new Button { 
+                Content = "❌", 
+                VerticalAlignment = VerticalAlignment.Center, 
+                Width = 20, 
+                Cursor = Cursors.Hand,
+                Background = Brushes.Transparent,
+                BorderThickness = new Thickness(0),
+            };
+            button.Click += RemoveItem;
+            DockPanel.SetDock(button, Dock.Right);
+
+            TextBlock textBlock = new TextBlock { Text = text, VerticalAlignment = VerticalAlignment.Center };
+            DockPanel.SetDock(textBlock, Dock.Left);
+
+            dockPanel.Children.Add(button);
+            dockPanel.Children.Add(textBlock);
+
+            return dockPanel;
+        }
+
+        private void AddItem(object sender, RoutedEventArgs e) {
+            ComboBox comboBox = (ComboBox)sender;
+            ComboBoxItem selectedItem = (ComboBoxItem)comboBox.SelectedItem;
+            if (comboBox == null || selectedItem == null) { return; }
+
+            // Get the parent of comboBox
+            DependencyObject parent = VisualTreeHelper.GetParent(comboBox);
+            while (parent != null && parent is not AttributesComboBox)
+            {
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+            if (parent == null) { return; }
+
+            String selectedItemContent = (String)selectedItem.Content;
+            AttributesComboBox parentAttributeCombo = (AttributesComboBox)parent;
+
+            DockPanel itemToAdd = CreateItem(selectedItemContent);
+            switch (parentAttributeCombo.Name) {
+                case "SingleAddCombo":
+                    if (IsInList(itemToAdd, SingleAddList)) { break; }
+                    SingleAddList.Items.Add(itemToAdd);
+                    break;
+                case "SingleRemoveCombo":
+                    if (IsInList(itemToAdd, SingleRemoveList)) { break; }
+                    SingleRemoveList.Items.Add(itemToAdd);
+                    break;
+                case "FolderAddCombo":
+                    if (IsInList(itemToAdd, FolderAddList)) { break; }
+                    FolderAddList.Items.Add(itemToAdd);
+                    break;
+                case "FolderRemoveCombo":
+                    if (IsInList(itemToAdd, FolderRemoveList)) { break; }
+                    FolderRemoveList.Items.Add(itemToAdd);
+                    break;
+                default: break;
+            }
+
+            // Reset selection to default value, not needed anymore
+            // comboBox.SelectedIndex = -1;
+        }
+
+        private void RemoveItem(object sender, RoutedEventArgs e) {
+            Button button = (Button)sender;
+            if (button == null) { return; }
+
+            // Get DockPanel ref from Button
+            DependencyObject parentDockPanel = VisualTreeHelper.GetParent(button);
+            while (parentDockPanel != null && parentDockPanel is not DockPanel) {
+                parentDockPanel = VisualTreeHelper.GetParent(parentDockPanel);
+            }
+            if (parentDockPanel == null) { return; }
+            DockPanel itemToDelete = (DockPanel)parentDockPanel;
+
+            // Get ListBox ref from StackPanel
+            DependencyObject parentList = VisualTreeHelper.GetParent(itemToDelete);
+            while (parentList != null && parentList is not ItemsControl)
+            {
+                parentList = VisualTreeHelper.GetParent(parentList);
+            }
+            if (parentList == null) { return; }
+            ItemsControl list = (ItemsControl)parentList;
+
+            list.Items.Remove(itemToDelete);
+        }
+
+        private static bool IsInList(DockPanel toFind, ItemsControl list) {
+            bool alreadyInList = false;
+
+            foreach (DockPanel item in list.Items)
+            {
+                TextBlock toFindText = (TextBlock)toFind.Children[1];
+                TextBlock currentListItem = (TextBlock)item.Children[1];
+                if (toFindText.Text.Equals(currentListItem.Text)) { alreadyInList = true; }
+            }
+
+            return alreadyInList;
         }
     }
 }
